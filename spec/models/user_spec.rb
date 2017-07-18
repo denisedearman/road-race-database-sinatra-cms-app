ea2f37f41be9f5e234ec6813d0f3a367756324bc
denisedearman <dearmandenise@gmail.com>
2017-07-18 12:03:39 -0400

    add tests to find by slug and get slug and username in the models/user_spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,20 @@ require 'spec_helper'
 
 describe 'User' do
   before do
-    @user = User.create(username: "tester", email: "test@test.com", password: "test123")
+    @user = User.create(username: "tester 1", email: "test@test.com", password: "test123")
+  end
+
+  it 'has a username' do
+    expect(@user.username).to eq("tester 1")
+  end
+
+  it 'can slug a username' do
+    expect(@user.slug).to eq ("tester-1")
+  end
+
+  it 'can find a user by its slug' do
+    slug = @user.slug
+    expect(User.find_by_slug(slug).username).to eq(@user.username)
   end
 
   it 'has secure password' do
