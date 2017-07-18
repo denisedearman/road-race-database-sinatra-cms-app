51d1bffaa2929d42522ff917c246938e04427ff3
denisedearman <dearmandenise@gmail.com>
2017-07-18 17:03:28 -0400

    added tests that all fields required to signup. Added test that logged in user cannot signup

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -17,6 +17,52 @@ describe UserController do
       post '/signup', params
       expect(last_response).location.to include("/races")
     end
+
+    it 'does not allow a user to signup without a username' do
+      params = {
+        username: "",
+        email: "irunforlife@gmail.com",
+        password: "notagoodpassword"
+      }
+      post '/signup', params
+      expect(last_response).location.to include("/signup")
+    end
+
+    it 'does not allow a user to signup without an email' do
+      params = {
+        username: "nycrunner",
+        email: "",
+        password: "notagoodpassword"
+      }
+      post '/signup', params
+      expect(last_response).location.to include("/signup")
+    end
+
+    it 'does not allow a user to signup without a password' do
+      params = {
+        username: "nycrunner",
+        email: "irunforlife@gmail.com",
+        password: ""
+      }
+      post '/signup', params
+      expect(last_response).location.to include("/signup")
+    end
+
+    it 'does not allow a logged in user to view the signup page' do
+      user = User.create(username: "nycrunner", email: "irunforlife@gmail.com", password: "notagoodpassword")
+      params = {
+        username: "nycrunner",
+        email: "irunforlife@gmail.com",
+        password: "notagoodpassword"
+      }
+      post '/signup', params
+      session = {}
+      session[:user_id] = user.id
+      get '/signup'
+      expect(last_response).location.to include("/races")
+    end
+
+
   end
 
 
