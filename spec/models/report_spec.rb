91d68811d0c1a016e30f004a93b1af277accbe2d
denisedearman <dearmandenise@gmail.com>
2017-07-18 13:42:12 -0400

    added attribute tests for report

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -7,4 +7,34 @@ describe 'Report' do
 
     @report = Report.create(user: marathoner, race: bigsur,title: "Breathtaking views", score: 5, year: 2010, content: "My first marathon", runs_per_week: 5, miles_per_week: 45)
   end
+
+  it 'can be initialized' do
+    expect(@report).to be_an_instance_of(Report)
+  end
+
+  it 'has a title' do
+    expect(@report.title).to eq("Breathtaking views")
+  end
+
+  it 'has a score' do
+    expect(@report.score).to eq(5)
+  end
+
+  it 'has a year' do
+    expect(@report.year).to eq(2010)
+  end
+
+  it 'has content' do
+    expect(@report.content).to eq("My first marathon")
+  end
+
+  it 'has runs per week' do
+    expect(@report.runs_per_week).to eq(5)
+  end
+
+  it 'has miles per week' do
+    expect(@report.miles_per_week).to eq(45)
+  end
+
+  
 end
