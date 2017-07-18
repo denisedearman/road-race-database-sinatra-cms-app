d57f983cb7676326f0e2dad034214b7efb575bdc
denisedearman <dearmandenise@gmail.com>
2017-07-18 13:14:36 -0400

    added tests for the race model attributes

--- spec/models/race_spec.rb
+++ spec/models/race_spec.rb
@@ -11,4 +11,19 @@ describe 'Race' do
     expect(@race).to be_an_instance_of(Race)
   end
 
+  it 'has a name' do
+    expect(@race.name).to eq("New York City Marathon")
+  end
+
+  it 'has a location' do
+    expect(@race.location).to eq("New York City, NY")
+  end
+
+  it 'has a next race day' do
+    expect(@race.next_race_day).to eq("November 5th, 2017")
+  end
+
+  it 'has a distance' do
+    expect(@race.distance).to eq("marathon")
+  end
 end
