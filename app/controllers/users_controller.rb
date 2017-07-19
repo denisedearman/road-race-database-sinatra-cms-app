c576ded9b23b91f03ebce3d5c7f5f73871691fd8
denisedearman <dearmandenise@gmail.com>
2017-07-19 12:13:59 -0400

    added login get and post methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
 
+  get '/login' do
+    if !logged_in?
+      erb :'/users/login'
+    else
+      redirect '/races'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/races'
+    else
+      redirect '/login'
+    end
+  end
+
   get '/signup' do
     if !logged_in?
       erb :'/users/new'
