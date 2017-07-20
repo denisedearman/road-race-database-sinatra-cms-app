a9508b81f5fe502272a19cb79ad767cb17e2569a
denisedearman <dearmandenise@gmail.com>
2017-07-20 11:39:04 -0400

    added test for checking that race index page works when logged in and doesn't when logged out.

--- spec/controllers/races_controller_spec.rb
+++ spec/controllers/races_controller_spec.rb
@@ -16,9 +16,25 @@ describe RacesController do
     context 'logged in' do
 
       it 'allows you to view all races' do
-        
-      end
+        user = User.create(:username => "trackstar", :email => "ilovetorun@aol.com", :password => "shoes")
 
+        visit '/login'
+
+        fill_in(:username, :with => "trackstar")
+        fill_in(:password, :with => "shoes")
+        click_button 'submit'
+        visit '/races'
+        expect(page.body).to include("St George Marathon")
+        expect(page.body).to include("Disney Princess Half Marathon")
+      end
+    end
+
+    context 'logged out' do
+
+      it 'does not allow you to view all races' do
+        visit '/races'
+        expect(last_response.location).to include('/login')
+      end
     end
 
   end
